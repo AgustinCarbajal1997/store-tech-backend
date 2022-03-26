2228b4d23a727deb78d058572c16251e4eae4516
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2022-03-25 23:33:52 -0300

    deploy

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const session = require("express-session");
 const cors = require("cors");
 const multer = require("multer");
 const storage = require("./config/multer");
-const config = require("./config");
 require("dotenv").config();
 const app = express();
 const { auth, products, carts, purchases } = require("./routes");
@@ -11,17 +9,14 @@ require("./db/mongo_connection");
 
 const PORT = process.env.PORT || 3000;
 
-// session setup
 app.use(cors());
 app.use(express.json());
 app.use(multer({ storage }).single("image"));
-app.use(session(config.session));
 
 app.use("/api/products", products);
 app.use("/api/auth", auth);
 app.use("/api/cart", carts);
 app.use("/api/purchase", purchases);
-// app.use("/api/payment", payment)
 app.listen(PORT, () => {
   console.log("Server running on PORT", PORT);
 });
